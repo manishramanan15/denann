878184d69c5ed388dd0fb2745bcc4880ceff7568
manishramanan15 <manishramanan15@gmail.com>
2016-11-16 19:35:55 +0530

    JS Bug fix

--- js/creative.js
+++ js/creative.js
@@ -61,7 +61,7 @@
             tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
             titleSrc: function (item) {
                 var data = $(item.el).data();
-                if (data.description) {
+                if (data.description && data.description!='') {
                     return '<p>' + data.description + ' <a href="' + data.url + '">More<i class="fa fa-icon fa-external-link"></i></a></p>';
                 }
                 else {
