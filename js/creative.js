462079eaab1cdb52c96b4609c544b1c0cacc9235
manishramanan15 <manishramanan15@gmail.com>
2016-11-29 13:33:54 +0530

    Image Carousel

--- js/creative.js
+++ js/creative.js
@@ -116,6 +116,21 @@
         });
     });
 
+
+    $(document).ready(function () {
+
+        $("#owl-demo").owlCarousel({
+
+            autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+            items: 4,
+            itemsDesktop: [1199, 3],
+            itemsDesktopSmall: [979, 3]
+
+        });
+
+    });
+
 })(jQuery); // End of use strict
 
 
